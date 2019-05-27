ea04a1d785afd6f595dcc3db348925a223a7d7ad
wtnb93 <wtnb93@users.noreply.github.com>
2019-05-27 12:24:16 +0900

    no message

--- run.py
+++ run.py
@@ -4,10 +4,7 @@ from pathlib import Path
 
 import cv2
 import m3u8
-from m3u8.model import Segment
 import ffmpeg
-import ffmpy
-import subprocess
 
 
 DEVICE_ID = 0
@@ -18,8 +15,6 @@ CAPTURE_DURATION = 10
 
 def main():
     vc = cv2.VideoCapture(DEVICE_ID)
-    # HACK: opencvが30FPSで書き出してくれない。
-    # fps = vc.get(cv2.CAP_PROP_FPS)
     width = int(vc.get(3))
     height = int(vc.get(4))
 
@@ -63,40 +58,27 @@ def generate_segment(filepath):
     Args:
         filepath: input video filepath
     Return:
-        Segm    ent object
+        m3u8.model.Segment object
     """
-    # segment_filepath = 'output/{}'.format(uuid.uuid4())
     filename = Path(filepath).name
     segment_filepath = f'output/{filename}'
 
-    # NOTE: ffmpeg-pythonをやめてsubprocessでやる。
-    # セグメントのサイズを知る必要がある。ffmpeg-pythonのm3u8に記述されるが1ファイル。
-    # subprocess.run(
-    #     f'ffmpeg -i {filepath} -vcodec libx264 -movflags faststart -vprofile baseline -level 3.0 -g 150 -b:v 519k -s 768x432 -acodec libfdk_aac -b:a 63.4k -ar 44100 -flags +loop-global_header -map 0 -bsf h264_mp4toannexb -f segment -segment_format mpegts -segment_time 10 -segment_list segment.m3u8 segment.ts',
-    #     shell=True
-    # )
-
-    # return m3u8.load(f'{segment_filepath}.m3u8').segments[0]
-
-    # HACK: segmentが複数作成された場合に対応する必要がある。
-    input_stream = ffmpeg.input(filepath, f='mp4')
-    output_stream = ffmpeg.output(input_stream, f'{segment_filepath}.m3u8', format='hls', hls_time=10, hls_list_size=3)
+    # HACK: videoはCAPTURE_DURATIONおきに書き出すが、
+    # 生成されるセグメントファイルは1つとは限らない(?)
+    # 複数作成される場合に対応する必要がある。
+    # どこかのタイミングで不要になった.mp4 .ts .m3u8ファイルを削除する必要がある。
+    input_stream = ffmpeg.input(filepath, format='mp4')
+    output_stream = ffmpeg.output(
+        stream=input_stream,
+        filename=f'{segment_filepath}.m3u8',
+        format='hls',
+        hls_time=CAPTURE_DURATION,
+        hls_list_size=3,
+    )
     ffmpeg.run(output_stream)
 
     playlist = m3u8.load(f'{segment_filepath}.m3u8')
     return playlist.segments[0]
-
-    # ff = ffmpy.FFmpeg(
-    #     inputs={filepath: None},
-    #     outputs={segment_filepath: None},
-    # )
-    # ff.run()
-
-    # return Segment(
-    #     uri=segment_filepath,
-    #     base_uri='',
-    #     duration=CAPTURE_DURATION,  # TODO: 実際の長さにする
-    # )
 
 
 def update_playlist(segment):
